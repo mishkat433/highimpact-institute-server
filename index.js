b627e4289bbdb3cba0ab403f73e99a40b710cda3
miskat433 <miskat433@gmail.com>
2022-10-26 11:33:26 +0600

    some route and data added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ PORT = 5000;
 app.use(cors())
 
 const category = require("./data/category.json");
+const cources = require("./data/cources.json");
+const team = require("./data/mentor.json");
 
 app.get("/", (req, res) => {
     res.send("server is running")
@@ -13,6 +15,13 @@ app.get("/", (req, res) => {
 
 app.get("/category", (req, res) => {
     res.send(category)
+})
+
+app.get("/cources", (req, res) => {
+    res.send(cources)
+})
+app.get("/team", (req, res) => {
+    res.send(team)
 })
 
 app.listen(PORT, () => {
